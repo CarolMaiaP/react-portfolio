f77965a5ea4edf488967e81f9ba5e534f18ea768
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-07-20 19:00:22 -0300

    social network component created

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,13 @@
 import Profile from '../assets/profile.jpg'
 import '../styles/components/sidebar.sass'
+import { SocialNetwork } from './SocialNetworks'
 
 export function Sidebar(){
   return(
     <aside id="sidebar">
       <img src={Profile} alt="Caroline Maia" />
       <p className="title">Desenvolvedora Front-end</p>
-      <p>Redes sociais</p>
+      <SocialNetwork/>
       <p>Informações de contato</p>
       <a href="#" className="btn">Download currículo</a>
     </aside>
