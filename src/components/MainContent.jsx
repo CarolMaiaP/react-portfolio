a3c5260620e2274e937af514a92f1c655577f78d
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-07-21 12:57:20 -0300

    projects component created

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,12 +1,13 @@
 import '../styles/components/mainContent.sass'
 import { AboutContainer } from './AboutContainer'
+import { ProjectsContainer } from './ProjectsContatiner'
 
 export function MainContent(){
   return(
     <main id='main-content'>
       <AboutContainer/>
       <p>tecnologias</p>
-      <p>projetos</p>
+      <ProjectsContainer/>
     </main>
   )
 }
---
+++ src/components/ProjectsContatiner.jsx
@@ -0,0 +1,9 @@
+export function ProjectsContainer(){
+  return(
+    <section className="projects-container">
+      <h2>Projetos</h2>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae cum eum non ad voluptatem perspiciatis fugiat nemo incidunt nam sapiente, laboriosam similique totam quam culpa maxime rem aliquid adipisci distinctio.</p>
+      <a href="#" className="btn">Ver Projetos</a>
+    </section>
+  )
+}
