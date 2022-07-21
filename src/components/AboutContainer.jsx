e0809a27f885650d82a2f4edd8a7d2a0807a6234
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-07-21 12:43:36 -0300

    about container component created

---
+++ src/components/AboutContainer.jsx
@@ -0,0 +1,11 @@
+import '../styles/components/aboutContainer.sass'
+
+export function AboutContainer(){
+  return(
+    <section className='about-container'>
+      <h2>Sobre</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus eaque dolores velit illo dignissimos. Beatae, itaque omnis provident minus doloribus iure obcaecati, fugit nisi ipsam non vel laborum nam exercitationem?</p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum, corrupti quidem assumenda qui voluptate, accusamus nesciunt dignissimos illo dolore ex unde vero voluptatum expedita veniam et enim reprehenderit, quia at?</p>
+    </section>
+  )
+}
--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,7 +1,12 @@
 import '../styles/components/mainContent.sass'
+import { AboutContainer } from './AboutContainer'
 
 export function MainContent(){
   return(
-    <main id='main-content'>MainContent</main>
+    <main id='main-content'>
+      <AboutContainer/>
+      <p>tecnologias</p>
+      <p>projetos</p>
+    </main>
   )
 }
